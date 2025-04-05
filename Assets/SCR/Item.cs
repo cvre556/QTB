93345ec7d9921e583582f660ae4d46576f2783de
cvre556 <cvrexz556@gmail.com>
2025-04-05 20:28:47 +0900

    making items
    add item, add item Light, add item Particle, enum Type 선언, PreFab Save

---
+++ Assets/SCR/Item.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type
+    {
+        Ammo, coin, Grenade, Heart, Weapon
+    };
+    public Type type;
+    public int value;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
