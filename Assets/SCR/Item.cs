cd69f74a5ef8fb7b29a138ecd89df0c74379abd1
cvre556 <cvrexz556@gmail.com>
2025-04-06 22:22:28 +0900

    Object Interation
    문제가 뭔지 모르겠어요!

--- Assets/SCR/Item.cs
+++ Assets/SCR/Item.cs
@@ -2,10 +2,7 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public enum Type
-    {
-        Ammo, coin, Grenade, Heart, Weapon
-    };
+    public enum Type{ Ammo, Coin, Grenade, Heart, Weapon };
     public Type type;
     public int value;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
